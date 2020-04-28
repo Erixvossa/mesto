b3c5155b9fd2e55e06dea2c2f5ec5824318d4bb2
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-04-28 15:18:11 +0300

    dev/new-feature/template

--- pages/index.js
+++ pages/index.js
@@ -67,4 +67,44 @@ function formSubmitHandler (evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 
 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+
+const elementTemplate = document.querySelector('#element').content;
+const elementsContainer = document.querySelector('.elements');
+const elementShow = elementTemplate.cloneNode(true);
+
+elementShow.querySelector('.element__title').textContent = initialCards[0].name;
+elementShow.querySelector('.element__photo').src = initialCards[0].link;
+
+elementsContainer.append(elementShow);
+
+
+
 
